215cd30eec90b55d7fc8d6459df848d516448281
sand500 <sand500@gmail.com>
2015-05-09 03:04:47 -0400

    Bot uses raw.js framework to a comment stream

--- bot.js
+++ bot.js
@@ -1,24 +1,44 @@
-var reddit = require('raw.js');
-var CommentStream = reddit.CommentStream;
-var x = 0;
-var stream = new CommentStream();
+var rawjs = require('raw.js');
+var reddit = new rawjs("raw.js example script");
 
-stream.on('comment', function(comment) {
-    //console.log("New comment in subreddit " + comment.subreddit + ", content " + comment.body);
-    	console.log(JSON.stringify(comment));
-	if(x == 0) {  
-    	x++;
-    }
-	this.stop();
+var fs = require('fs');
+fs.readFile('secret.txt','utf8',function (err, data) {
+	if (err) throw err;
+	var info = data.split(" ");
+	reddit.setupOAuth2(info[0], info[1], "http://www.example.com/redditoauth");
+	console.log("'"+info[0]+"'\n'"+info[1]+"'\n'"+info[2]+"'\n'"+info[3]+"'\n");
+	
+
+	var CommentStream = rawjs.CommentStream;
+	var stream = new CommentStream();
+    var x = 0;
+	stream.on('comment', function(comment) {
+	    //console.log("New comment in subreddit " + comment.subreddit + ", content " + comment.body);
+	    if(comment.author.toLowerCase().indexOf("sand500")!=-1){
+	    	console.log(comment.body+"\n");
+	    }
+	     if(comment.body.toLowerCase().indexOf("buzzfeed")!=-1){
+	    	console.log(comment.body+"\n");
+	    }
+	    x++;
+	    if(x%1000 == 0){
+	    	console.log(x);
+		}
+		
+		
+	});
+
+	stream.on('error', function(e) {
+	    console.log("Error: " + e);
+	});
+
+  
 });
 
-stream.on('error', function(e) {
-    console.log("Error: " + e);
-});
 
 /*
 var request = require("request");
-var fs = require('fs');
+
 console.log("hi");
 var cc= "http://www.reddit.com/r/all/.json"
     request(cc, function (error, response, body) {
